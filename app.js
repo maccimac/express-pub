7a7d406f531d23a966a8cdc0bb8d98294832c648
maccimac <macci.web@gmail.com>
2023-02-26 15:47:24 -0800

    Use json; Add some content

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// const data = require('./data.json');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -6,44 +7,15 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(express.static(__dirname))
 app.set('view engine', 'pug')
 
-// const names = [
-//     'Joshua',
-//     'Umar',
-//     'Julia',
-//     'Simran'
-// ]
+var fs = require('fs'); /* Put it where other modules included */
+var data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
 
-const projects = [
-    {
-        title: 'Trickle.app',
-        description: 'Online learning platform',
-        url: 'http://trickle.app',
-        tags: ['Development'],
-        contributions: '',
-        notes: 'Lorem ipsum',
-        img: '/public/img/skyline.jpg'
-    },
-    {
-        title: 'THINC',
-        description: 'Lawyer ',
-        url: 'http://trickle.app',
-        tags: ['Development'],
-        contributions: '',
-        notes: 'Lorem ipsum',
-        img: '/public/img/glasses.jpg'
-    }
+const {projects, skills} = data;
 
-]
 
 app.get('/', (req,res)=>{
-    res.render('index', {projects})
+    res.render('index', {projects: data.projects})
 })
-
-// app.get('/project', (req,res)=>{
-//     // const project  = projects[req.params.id]
-//     // res.render('project ', {project})
-//     res.render('project ', {projects})
-// })
 
 app.get('/project/:i', (req,res)=>{
     const project  = projects[req.param("i")]
@@ -51,13 +23,8 @@ app.get('/project/:i', (req,res)=>{
 })
 
 app.get('/about', (req,res)=>{
-    res.render('about')
+    res.render('about', {skills})
 })
-
-// app.post('/', (req, res) => {
-//     names.push(req.body.name)
-//     res.render('index', { names });
-//   });
 
 app.listen(3000, ()=>{
     console.log('Server has started at 3000. Pug is used in this server render.')
---
+++ data.js
@@ -0,0 +1,41 @@
+const skills = [
+    "JavaScript",
+    "Vue.js, ReactJS",
+    "Bootstrap, SASS",
+    "jQuery, AJAX — Typescript",
+    "WordPress — PHP — Node.js, Express",
+    "MySQL, MongoDB — Angular, Jasmine",
+    "Laravel",
+    "FTP, CPanel",
+    "HTML5, CSS3 — C#, Java",
+    "Software Development Life Cycle: Development, Testing, Deployment",
+    "GitHub,  Gitlab, Gulp",
+    "Adobe Suite (XD, Photoshop, Illustrator), Figma, Sketch"
+]
+
+
+const projects = [
+    {
+        title: 'Trickle.app',
+        description: 'Online learning platform',
+        url: 'http://trickle.app',
+        tags: ['Development'],
+        contributions: '',
+        notes: 'Lorem ipsum',
+        img: '/public/img/skyline.jpg'
+    },
+    {
+        title: 'THINC',
+        description: 'Lawyer ',
+        url: 'http://trickle.app',
+        tags: ['Development'],
+        contributions: '',
+        notes: 'Lorem ipsum',
+        img: '/public/img/glasses.jpg'
+    }
+]
+
+
+module.exports = {
+    projects, skills
+  }
